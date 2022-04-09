71b77e3044c8e7be217be05fc5c5caff2915aa9b
Krysoo <krysoprestige@googlemail.com>
2022-04-09 23:31:10 +0200

    changed icon

--- components/Form.js
+++ components/Form.js
@@ -124,7 +124,7 @@ class Form extends React.Component {
             <div>
                 <Container component="main">
                     <Box sx={{marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                    <Avatar src="../lock2.png" sx={{width: 56, height: 56, marginBottom: "1.5vh"}} title="Icon created by rizky adhitya pradana (https://www.flaticon.com/free-icon/lock_6388589)"></Avatar>
+                    <Avatar src="../lock.png" sx={{width: 56, height: 56, marginBottom: "1vh"}} title="Icon created by Uniconlabs (https://www.flaticon.com/free-icon/locked_6815095)"></Avatar>
                         <FormControl sx={{ width: '40ch' }}>
                             <Typography align="center" component="h1" variant="h5">
                                 Formularz
